10594361f911fe60d2e641bfbcc4006beb9cfd64
Bryan Swift <mail@bryanwrit.es>
2014-03-17 10:15:42 -0700

    Download the right hk bash completion script

--- bash-completion/hk-bash-completion.sh
+++ bash-completion/hk-bash-completion.sh
@@ -1,1 +1,23 @@
-<html><body>You are being <a href="https://raw.github.com/heroku/hk/master/contrib/hk-bash-completion.sh">redirected</a>.</body></html>
+#!/bin/bash
+
+_hk_commands()
+{
+    hk help commands|cut -f 2 -d ' '
+}
+
+_hk()
+{
+    cur=${COMP_WORDS[COMP_CWORD]}
+    prev=${COMP_WORDS[COMP_CWORD-1]}
+    if [ $COMP_CWORD -eq 1 ]; then
+        COMPREPLY=( $( compgen -W "$(_hk_commands)" $cur ) )
+    elif [ $COMP_CWORD -eq 2 ]; then
+        case "$prev" in
+        help)
+            COMPREPLY=( $( compgen -W "$(_hk_commands)" $cur ) )
+            ;;
+        esac
+    fi
+}
+
+complete -F _hk -o default hk
